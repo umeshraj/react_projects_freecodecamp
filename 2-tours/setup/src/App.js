2f1f3b9307d78ba95b98def3e6b8c25826d42152
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-11-27 05:31:58 -0500

    Remove tour via prop drilling

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -9,6 +9,12 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  // remove tour
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   //fetch data
   const fetchTours = async () => {
     setLoading(true);
@@ -35,7 +41,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
