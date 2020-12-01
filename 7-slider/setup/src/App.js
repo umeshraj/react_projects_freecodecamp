c906f9090115cbe9f5077744ab0008e336a3b3a5
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-01 05:29:45 -0500

    Adding class for prev/next slide and auto slide

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,23 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className="section">
       {/* title */}
@@ -41,10 +58,20 @@ function App() {
           );
         })}
 
-        <button className="prev">
+        <button
+          className="prev"
+          onClick={() => {
+            setIndex(index - 1);
+          }}
+        >
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button
+          className="next"
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+        >
           <FiChevronRight />
         </button>
       </div>
