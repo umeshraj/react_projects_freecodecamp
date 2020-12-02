07658fc3e337ef305235d267257001d122a291cc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-02 04:57:10 -0500

    Setting up basic skeleton

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data);
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs: </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      {/* space for text */}
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi,
+          molestias?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+          doloremque laborum deleniti? Reprehenderit, tempore.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
