ee86b7089e14d53773c504c5b4f24f53bf9527ec
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-03 05:21:03 -0500

    Setting up default values

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,9 +4,9 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState("#f15025");
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
