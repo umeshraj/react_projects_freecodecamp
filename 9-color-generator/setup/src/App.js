9d75e298207babd068676c34e945de30d3acd08c
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-03 05:11:55 -0500

    Showing hex value

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,7 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("#f15025");
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
@@ -36,9 +36,16 @@ function App() {
         </form>
       </section>
       ;
-      <section className="color">
+      <section className="colors">
         {list.map((color, index) => {
-          return <SingleColor key={index} {...color} index={index} />;
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
         })}
       </section>
     </>
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
-const SingleColor = ({ rgb, weight, index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  console.log(hexColor);
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+
   console.log(bcg);
   return (
     <article
-      className={`color`}
+      className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
-    ></article>
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+    </article>
   );
 };
 
