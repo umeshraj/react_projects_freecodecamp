cbf5f9c79e0986f98d7f05acf0ce859335d7a29e
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-06 05:31:08 -0500

    Auto adjust navbar height

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,6 +5,19 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  // height of links
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
 
   return (
     <nav>
@@ -21,12 +34,8 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
