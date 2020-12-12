1b604d765be436ca1820ce1fce01e7630dbc4f0d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-12 11:41:53 -0500

    Handle case where no drinks were fetched

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -6,8 +6,29 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [searchTerm, setSearchTerm] = useState("a");
+  const [searchTerm, setSearchTerm] = useState("axxx");
   const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
+
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
       {children}
