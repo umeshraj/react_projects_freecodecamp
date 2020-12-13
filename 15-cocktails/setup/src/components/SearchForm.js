dd7f7d4ceed5d40c83d126068a5a70ba75e289e8
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-12-13 05:40:05 -0500

    Prevent loading when blank

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -12,9 +12,13 @@ const SearchForm = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktails</label>
           <input
